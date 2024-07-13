3580f8a8574b8bdfc5c040c3d1b2b08e849a9ebd
tanishq bhatia <tanishqbhatia@Tanishqs-MacBook-Air-M1.local>
2024-07-13 22:35:32 +0530

    initial

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">DineEase</div>
+          <div className="right">
+            <p>89/270,Block M, Connaught Place, New Delhi,</p>
+            <p>Open: 05:00 PM - 2:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <a href="https://www.linkedin.com/in/tanishq-bhatia-371641244/">Tanishq Bhatia</a>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Tanishqbhatia</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
